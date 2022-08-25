6011d22f92c8335c06c9e35c7fcdff7d5fab864f
shashank singhal <shashanksinghal883@gmail.com>
2022-08-25 18:02:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,66 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Particles from 'react-particles-js';
-import Navbar from './Components/Navbar';
-import Header from './Components/Header';
-import Aboutme from './Components/Aboutme';
-import Experience from './Components/Experience';
-import Portfolio from './Components/Portfolio';
-import Form from './Components/Contact';
-import Footer from './Components/Footer';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import Aboutme from "./Components/Aboutme";
+import Experience from "./Components/Experience";
+import Portfolio from "./Components/Portfolio";
+import Form from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 // import Slide from './Component/Slide';
 // import Box from './Components/three-scene';
 // import Threejs from './components/Three';
 // import Img from './Components/three-scene';
 
-
-
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+    // Aos.refresh();
+  }, []);
   return (
     <>
-    <Particles 
-    className="particles-canvas"
-    params={{
-      particles:{
-        number:{
-          value:30,
-          density: {
-            enable: true, 
-            value_area:700
-          }
-        },
-        shape:{
-          type:"circle",
-          stroke:{
-            width:6,
-            color:"#f9ab00" 
-          }
-        }
-      }
-    }}
-    />
-  <Navbar/>
-  <Header/>
-  <Aboutme/>
-  {/* <Resume/> */}
-  <Experience/>
-  {/* <Img/> */}
-  {/* <ThreeScene /> */}
-  <Portfolio/>
-  {/* <Slide/> */}
-  {/* <Contact/> */}
-   <Form/>
-  <Footer/>
-  </>
-
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 700,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+      <Aboutme />
+      {/* <Resume/> */}
+      <Experience />
+      {/* <Img/> */}
+      {/* <ThreeScene /> */}
+      <Portfolio />
+      {/* <Slide/> */}
+      {/* <Contact/> */}
+      <Form />
+      <Footer />
+    </>
   );
 }
 
 export default App;
- 
-
 
 // const serviceID = "service_wc8r197"
 // const templateID = "template_0o894hc"
--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,84 +1,124 @@
 // import React from "react"
 import js from "../js.png";
-import Html from '../Components/three-scene'
-import Css from '../Components/Css3d'
+import Html from "../Components/three-scene";
+import Css from "../Components/Css3d";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import {useEffect } from "react";
+import { useEffect } from "react";
 // import html5 from "../html5.png";
-import react from "../logo.svg"
+import react from "../logo.svg";
 // import css3 from "../css3.png"
 
-    const Experience = () => {
-        useEffect(() => {
-          Aos.init({ duration: 2000 });
-        }, []);
-    return (
-        <div id="experience" className="experience">
-            <div className="d-flex justify-content-center my-5">
-                <h1>Experience</h1>
-            </div>
-            <div className="container experience-wrapper">
-                <div className="timeline-block timeline-block-right">
-                    <div className="marker"></div>
-                    <div className="timeline-content">
-                        <h3 data-aos="fade-left"> React Application (September 2021 – Working)</h3>
-                        <p data-aos="fade-left" className="firstjob">
-                            <br></br>● Fresher in React Developer with hands-on experience in identifying web-based user interactions along with designing & implementing highly-responsive user interface components by deploying React concepts.
-                            <br></br>● Proficient in translating designs & wireframes into high-quality code, and writing application interface code via Javascript and ReactJS workflows.
-                            <br></br>● Adept at monitoring & maintaining frontend performance and troubleshooting & debugging the same to bolster overall performance.
-                            <br></br>● Worked on a react app that allows users to add, edit, and delete tasks they want to work on, and save in local storage either the app opens in browser or not.
-                            <br></br>● understanding a basic react concepts of hooks , rendering, props , virtual DOM, router, usestate, etc.
-                            <br></br>● Knowledge of Threejs js used to create and display animated 3D computer graphics in a web browser using WebGL.
-                        </p>
-                    </div>
-                </div>
-                            <img data-aos="fade-right" className = "react-img" src = {react} alt = "react" />
-                {/* second job */}
-                <div className="timeline-block timeline-block-left">
-                    <div className="marker"></div>
-                    <div className="timeline-content ">
-                        <h3 data-aos="fade-right">JavaScript (Jun 2021 – August 2021)</h3>
-                        <p data-aos="fade-right" className="secondjob">
-                            
-                            <br></br>● Fresher in JavaScript Developer with necessary perception and heavily Knowledge of javascript.
-                            <br></br>● Worked on a weather App Using Javascript features, the user can estimate the current-day climatic probability and determine whether the day will be cloudy or sunny. In the software, the user can write down the name of any city in the world. 
-                            <br></br>● This project also includes a significant amount of javascript to ensure the project’s functionality Scope.                            
-                            {/* <br></br>● IIFE (Immediately Invoked Function Expression) As the name suggests IIFE is a function in javascript which immediately invoked and executed as soon as it is defined.   */}
-                            <br></br>● Operate the concepts of rest API's and fetch the details. 
-                            <br></br>● Working with Hoisting,Closures,Callbacks,Promises.
-                            <br></br>● Knowledge of JavaScript(ES6) Class,Prototype,Async & Await.
-                            <br></br>● intellect with Scope,JavaScript Closures, The Module Pattern.
-                        </p>
-                    </div>
-                </div>
-                        <img data-aos="fade-left" className = "js-img timeline-block timeline-block-right" src = {js} alt = "JavaScript" />
-                {/* third job */}
-                <div className="timeline-block timeline-block-right">
-                    <div className="marker"></div>
-                    <div className="timeline-content">
-                        <h3 data-aos="fade-left"> HTML and CSS (March 2021 - May 2021)</h3>
-                        <p data-aos="fade-left" className="thirdjob">
-                            <br></br>● Created a simple yet very useful and well-maintained site is developed with HTML tags. The whole project is designed in HTML ,CSS and JavaScript language.
-                            <br></br>● Different tags have been used for the development in the project.
-                            <br></br>● Functioning with all essential and usefull tags in HTML.
-                            <br></br>● The syntax and forms of the language, Specificity, inheritance, and the Cascade, CSS units and values and functional notationsBox model and margin collapse are used in project.
-                            <br></br>● They containing block, Stacking and block-formatting contexts, Initial, computed, used, and actual values.
-                            <br></br>● CSS shorthand properties, CSS Flexible Box Layout, CSS Grid Layout, CSS selectors, Media queries, Animation are used in project.
-                            {/* <br></br>● */}
-                            {/* <br></br>● */}
-                             <Html data-aos="fade-up" />
-                             <Css data-aos="fade-up"/>
-                        </p>
-                    </div>
-                </div>
-                            {/* <img className = "html5-img " src = {html5} alt = "html5" />  */}
-                            {/* <img className = "css3-img " src = {css3} alt = "css3" /> */}
-                {/* fourth job */}
-
-            </div>
+const Experience = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  return (
+    <div id="experience" className="experience">
+      <div className="d-flex justify-content-center my-5">
+        <h1>Experience</h1>
+      </div>
+      <div className="container experience-wrapper">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3 data-aos="fade-left">
+              {" "}
+              React Application (September 2021 – Working)
+            </h3>
+            <p data-aos="fade-left" className="firstjob">
+              <br></br>● Fresher in React Developer with hands-on experience in
+              identifying web-based user interactions along with designing &
+              implementing highly-responsive user interface components by
+              deploying React concepts.
+              <br></br>● Proficient in translating designs & wireframes into
+              high-quality code, and writing application interface code via
+              Javascript and ReactJS workflows.
+              <br></br>● Adept at monitoring & maintaining frontend performance
+              and troubleshooting & debugging the same to bolster overall
+              performance.
+              <br></br>● Worked on a react app that allows users to add, edit,
+              and delete tasks they want to work on, and save in local storage
+              either the app opens in browser or not.
+              <br></br>● understanding a basic react concepts of hooks ,
+              rendering, props , virtual DOM, router, usestate, etc.
+              <br></br>● Knowledge of Threejs js used to create and display
+              animated 3D computer graphics in a web browser using WebGL.
+            </p>
+          </div>
         </div>
-    )
-}
+        <img
+          data-aos="fade-right"
+          className="react-img"
+          src={react}
+          alt="react"
+        />
+        {/* second job */}
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content ">
+            <h3 data-aos="fade-right">JavaScript (Jun 2021 – August 2021)</h3>
+            <p data-aos="fade-right" className="secondjob">
+              <br></br>● Fresher in JavaScript Developer with necessary
+              perception and heavily Knowledge of javascript.
+              <br></br>● Worked on a weather App Using Javascript features, the
+              user can estimate the current-day climatic probability and
+              determine whether the day will be cloudy or sunny. In the
+              software, the user can write down the name of any city in the
+              world.
+              <br></br>● This project also includes a significant amount of
+              javascript to ensure the project’s functionality Scope.
+              {/* <br></br>● IIFE (Immediately Invoked Function Expression) As the name suggests IIFE is a function in javascript which immediately invoked and executed as soon as it is defined.   */}
+              <br></br>● Operate the concepts of rest API's and fetch the
+              details.
+              <br></br>● Working with Hoisting,Closures,Callbacks,Promises.
+              <br></br>● Knowledge of JavaScript(ES6) Class,Prototype,Async &
+              Await.
+              <br></br>● intellect with Scope,JavaScript Closures, The Module
+              Pattern.
+            </p>
+          </div>
+        </div>
+        <img
+          data-aos="fade-left"
+          className="js-img timeline-block timeline-block-right"
+          src={js}
+          alt="JavaScript"
+        />
+        {/* third job */}
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3 data-aos="fade-left"> HTML and CSS (March 2021 - May 2021)</h3>
+            <p data-aos="fade-left" className="thirdjob">
+              <br></br>● Created a simple yet very useful and well-maintained
+              site is developed with HTML tags. The whole project is designed in
+              HTML ,CSS and JavaScript language.
+              <br></br>● Different tags have been used for the development in
+              the project.
+              <br></br>● Functioning with all essential and usefull tags in
+              HTML.
+              <br></br>● The syntax and forms of the language, Specificity,
+              inheritance, and the Cascade, CSS units and values and functional
+              notationsBox model and margin collapse are used in project.
+              <br></br>● They containing block, Stacking and block-formatting
+              contexts, Initial, computed, used, and actual values.
+              <br></br>● CSS shorthand properties, CSS Flexible Box Layout, CSS
+              Grid Layout, CSS selectors, Media queries, Animation are used in
+              project.
+              {/* <br></br>● */}
+              {/* <br></br>● */}
+              <Html data-aos="fade-up" />
+              <Css data-aos="fade-up" />
+            </p>
+          </div>
+        </div>
+        {/* <img className = "html5-img " src = {html5} alt = "html5" />  */}
+        {/* <img className = "css3-img " src = {css3} alt = "css3" /> */}
+        {/* fourth job */}
+      </div>
+    </div>
+  );
+};
 
 export default Experience;
