597db76fe5b11016e7d049fee5e8a90107e04a6a
shashank singhal <shashanksinghal883@gmail.com>
2023-03-15 01:36:48 +0530

    favicon added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,28 +3,34 @@ import Typed from "react-typed";
 import { Link } from "react-scroll";
 
 const Header = () => {
-    return (
-        <div id= "home" className="header-wrapper">
-            <div className="main-info">
-                <h1> web development and website design</h1>
-            
-                <Typed
-                className="typed-text"
-                strings= {["Web Design", "Web Development", "HTML","CSS","JAVASCRIPT", "REACTJS"]}
-                typeSpeed={50}
-                backSpeed={30}
-                
-                loop
-                
-                />
+  return (
+    <div id="home" className="header-wrapper">
+      <div className="main-info">
+        <h1> web development and website design</h1>
 
-                <Link smooth={true} to="contact"  className="btn-main-offer"  href="#" >Contact me </Link>
+        <Typed
+          className="typed-text"
+          strings={[
+            "Web Design",
+            "Web Development",
+            "HTML",
+            "CSS",
+            "JAVASCRIPT",
+            "REACTJS",
+            "NODEJS",
+            "MONGODB",
+          ]}
+          typeSpeed={50}
+          backSpeed={30}
+          loop
+        />
 
-            
-            </div>
-            
-        </div>
-    )
-}
+        <Link smooth={true} to="contact" className="btn-main-offer" href="#">
+          Contact me{" "}
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
