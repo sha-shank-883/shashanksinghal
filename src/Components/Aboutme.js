9fb503d11b87c69f8a176d179bf003a9861884fd
shashank singhal <shashanksinghal883@gmail.com>
2023-03-16 16:59:03 +0530

    changes

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
   return (
     <div id="about" className="container py-5">
       <div className="row">
-        <div className="col-lg-6 col-xm-12">
+        <div className="col-lg-6 col-md-5 col-12 col-xm-12">
           {/* mb-5 and other vlaues bootstrap values */}
           <div className="photo-wrap mb-5">
             <img
@@ -26,7 +26,7 @@ const AboutMe = () => {
             {/* <img className = "profile-img" src = {Me} alt = "author" /> */}
           </div>
         </div>
-        <div className="col-lg-6 col-xm-12">
+        <div className="col-lg-6 col-md-6 col-12 col-xm-12">
           <h1 data-aos="fade-up" className="about-heading">
             About Me
           </h1>
